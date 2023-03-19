ecadac0d64f9f7444364d8afda25b3b3bedce161
Mike Judge <mike@substantial.com>
2023-03-19 12:20:41 -0700

    Update setup.py to be aware of the new src directory

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    install_requires=requirements
+    name="tai",
+    version="0.1.0",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
 )
