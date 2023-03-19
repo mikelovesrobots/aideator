290b7f4e74361594d75ead2aef7b07f5bc4964de
Mike Judge <mike@substantial.com>
2023-03-19 13:29:30 -0700

    Rework the directory structure to be a little more classic python module-esque

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
     version="0.1.0",
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
+    entry_points={
+        'console_scripts': [
+            'tai = tai:main',
+        ],
+    },
     install_requires=requirements,
 )
